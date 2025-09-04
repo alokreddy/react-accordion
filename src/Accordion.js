8786f6748ff13b76a10ea9ba47db6a6ed66e4c77
alokreddy <alokreddyk@gmail.com>
2025-09-04 09:35:03 +0530

    refactor: update Accordion component structure and styles for consistency

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,40 +1,36 @@
 import { useState } from "react";
-import { GoPlus, GoHorizontalRule } from "react-icons/go";
 
 export default function Accordion({ sections }) {
-  const [openSections, setOpenSections] = useState(new Set());
+  const [openSections, setOpenSection] = useState(new Set());
 
   return (
     <div className="accordion">
       {sections.map(({ value, title, contents }) => {
         const isExpanded = openSections.has(value);
-        let openCloseIcon = !isExpanded ? <GoPlus /> : <GoHorizontalRule />;
         return (
-          <div className="accordion__item" key={value}>
+          <div className="accordion-item" key={value}>
             <button
-              className="accordion__item-title"
-              type="button"
+              className="accordion-item-title"
               onClick={() => {
                 const newOpenSections = new Set(openSections);
                 newOpenSections.has(value)
                   ? newOpenSections.delete(value)
                   : newOpenSections.add(value);
-                setOpenSections(newOpenSections);
+                setOpenSection(newOpenSections);
               }}
             >
-              <span className="accordion__item-title-text">{title}</span>
-              <span className="accordion__item-icon">{openCloseIcon}</span>
+              {title}
               <span
                 aria-hidden={true}
                 className={[
-                  "accordion__item-arrow",
-                  isExpanded && "accordion__item-arrow--rotated",
+                  "accordion-icon",
+                  isExpanded && "accordion-icon--rotated",
                 ]
                   .filter(Boolean)
                   .join(" ")}
               />
             </button>
-            <div className="accordion__item-contents" hidden={!isExpanded}>
+            <div className="accordion-item-contents" hidden={!isExpanded}>
               {contents}
             </div>
           </div>
